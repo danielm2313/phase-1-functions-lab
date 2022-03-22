a8fce5c83149ebc5ad8ee24730dd386bbc576a57
danielm2313 <daniel.margit config --global user.email daniel.martinez.00@hotmail.com>
2022-03-22 10:03:12 -0500

    commit tree 864119148a31ba5428b093a18562b5ae5cde4ffc

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+    return distance > 42 ? distance - 42 : 42 - distance
+}
+function distanceFromHqInFeet (distance) {
+    return distanceFromHqInBlocks (distance) * 264
+}
+function distanceTravelledInFeet (starting, ending) {
+    const result = (ending - starting) * 264
+    if(starting > ending)
